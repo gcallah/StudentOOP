cc4207bf6b27ba30b4388677909106f0f59cba6e
gcallah <gcallah@mac.com>
2019-06-02 02:55:36 -0400

    testing new code

--- my_code/pointers.h
+++ my_code/pointers.h
@@ -1,3 +1,18 @@
+#ifndef POINTERS_H
+#define POINTERS_H
+
+struct Thing {
+    int val;
+    Thing(int i) : val(i) {}
+};
+
 void inc_by_value(int n);
 void inc_with_pointer(int* n);
 void inc_with_reference(int& n);
+
+Thing** create_array_of_things(int n);
+void print_all_things(Thing** things, int n);
+void double_all_things(Thing** things, int n);
+void delete_all_things(Thing** things, int n);
+
+#endif
--- tests/test_pointers.cpp
+++ tests/test_pointers.cpp
@@ -3,7 +3,11 @@
 
 using namespace std;
 
+const int NUM_THINGS = 40;
+
+
 int main() {
+    // first we test the various methods of passing args:
     int n = 0;
     inc_by_value(n);
     assert(n == 0);
@@ -11,5 +15,20 @@ int main() {
     assert(n == 1);
     inc_with_reference(n);
     assert(n == 2);
+
+    // now let's make, print, change, and delete some "things":
+    Thing** things = create_array_of_things(NUM_THINGS);
+    assert(things != nullptr);
+
+    print_all_things(things, NUM_THINGS);
+
+    double_all_things(things, NUM_THINGS);
+    for (int i = 0; i < NUM_THINGS; i++)
+        assert(things[i]->val == (i * 2));
+
+    delete_all_things(things, NUM_THINGS);
+
+    // now assign TAs to students
+
     return 0;
 }
