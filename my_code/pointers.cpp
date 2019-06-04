9f24eda5ea6d01b698b82e4a9878db172f9fd347
gcallah <gcallah@mac.com>
2019-06-04 12:54:07 -0400

    testing new code

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -30,17 +30,8 @@ void delete_all_things(Thing** things, int n) {
 
 
 void assignTA(Student* s, Student* ta) {
-    s->ta = ta;
 }
 
 void printTAs(vector<Student*>& students) {
-    for (Student* student : students) {
-        if (student->ta == nullptr) {
-            cout << student->name << " has no TA\n";
-        }
-        else {
-            cout << student->name << " has TA " << student->ta->name << endl;
-        }
-    }
 }
 
