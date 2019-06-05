7ecc0c513fe26908be02b59e935e3003eeb5cee0
gcallah <gcallah@mac.com>
2019-06-05 08:55:49 -0400

    Added instructions to pointers.cpp.

--- my_code/pointers.cpp
+++ my_code/pointers.cpp
@@ -3,35 +3,65 @@
 
 using namespace std;
 
+/*
+ * Increment n.
+ * */
 void inc_by_value(int n) {
 }
 
 
+/*
+ * Increment n.
+ * */
 void inc_with_pointer(int* iptr) {
 }
 
 
+/*
+ * Increment n.
+ * */
 void inc_with_reference(int& n) {
 }
 
 
+/*
+ * Create an array of n things, each one with its val set to
+ * its index in the Thing array.
+ * */
 Thing** create_array_of_things(int n) {
     return nullptr;
 }
 
+/*
+ * Print all of the things created in the function above.
+ * */
 void print_all_things(Thing** things, int n) {
 }
 
+/*
+ * Double the value of each thing (use *=).
+ * */
 void double_all_things(Thing** things, int n) {
 }
 
+
+/*
+ * Delete all of the things, and the Thing array, that you have created.
+ * */
 void delete_all_things(Thing** things, int n) {
 }
 
 
+/*
+ * Assign student `s` the TA `ta`.
+ * */
 void assignTA(Student* s, Student* ta) {
 }
 
+
+/*
+ * Print who has which TA.
+ * */
 void printTAs(vector<Student*>& students) {
 }
 
