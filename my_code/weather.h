d2a3b08a79fc93599a391b05d74d44fc523bd619
gcallah <gcallah@mac.com>
2019-06-06 11:11:38 -0400

    Checking in for first weather lab.

--- my_code/weather.h
+++ my_code/weather.h
@@ -10,14 +10,24 @@ struct GPS {
         longitude(lo) {}
 };
 
+std::ostream& operator<<(std::ostream& os, const GPS& gps);
+
+const int UNRATED = -1;
+const int BAD = 0;
+const int OK = 1;
+const int GOOD = 2;
 
 class Weather {
+    friend std::ostream& operator<<(std::ostream& os, const Weather& w);
  public:
     Weather(std::string nm, GPS loc);
-    std::string get_name();
+    std::string get_name() const;
+    int get_rating() const;
+    void set_rating(int new_rating);
  private:
     std::string station_nm;
     GPS my_loc;
+    int rating = UNRATED;
 };
 
 
--- tests/test_weather.cpp
+++ tests/test_weather.cpp
@@ -7,5 +7,5 @@ using namespace std;
 int main() {
     GPS loc = GPS(46.3, 67.2);
     Weather irkutsk = Weather("Irkutsk", loc);
-    cout << irkutsk.get_name() << endl;
+    cout << irkutsk << endl;
 }
