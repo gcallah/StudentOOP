5438185bf6c791632f13814db8d20b6cad11a067
gcallah <gcallah@mac.com>
2019-06-11 11:49:27 -0400

    Taking out code for complex.cpp

--- my_code/complex.h
+++ my_code/complex.h
@@ -71,15 +71,8 @@ class Complex {
      * */
     Complex operator--(int dummy);
 
-    /*
-     * `+` adds real to real, and complex to complex.
-     * */
-    Complex operator+(const Complex& c);
 
-    /*
-     * `-` subtracts real from real, and complex from complex.
-     * */
-    Complex operator-(const Complex& c);
+    Complex operator-=(const Complex& c);
 
     /*
      * Let's do multiplication by a scalar:
@@ -94,5 +87,14 @@ class Complex {
 
 bool operator== (const Complex& c1, const Complex& c2);
 bool operator!= (const Complex& c1, const Complex& c2);
+/*
+ * `+` adds real to real, and complex to complex.
+ * */
+Complex operator+(const Complex& c1, const Complex& c2);
+
+/*
+ * `-` subtracts real from real, and complex from complex.
+ * */
+Complex operator-(const Complex& c1, const Complex& c2);
 
 #endif
--- tests/test_complex.cpp
+++ tests/test_complex.cpp
@@ -17,51 +17,63 @@ int main() {
         cerr << "Bad input format\n";
         exit(1);
     }
-    if(DEBUG)
-    {
-        cout << "c1 = " << c1 << endl;
-    }
-
-    Complex c2{43.2, 58.9};
-    assert(c2.get_real() == 43.2);
-    assert(c2.get_imag() == 58.9);
-
-    if(DEBUG)
-    {
-        cout << "c2 = " << c2 << endl;
-    }
-
-    // see if `Complex` addition works:
-    // Complex c3 = c1 + c2;
-    // see if `Complex` subtraction works:
-
-    Complex c4 = Complex();
-    // test pre-increment:
-    c4++;
-    c4++;
-    assert(c4.get_real() == 2);
-    Complex postinc = c4++;
-    assert(postinc.get_real() == 2);
-    /*
-     * Test equality operator. If we take the `explicit` off of
-     * `bool()` in class definition, then this will fail as ambiguous:
-     * the compiler won't know if we want a `bool` or `Complex` comparison.
-     * */
-    (1 == bool(c4)) ? cout << "c4 == 1 is true\n"
-        : cout << "c4 == 1 is false\n";
-    // test bool() operator:
-    (c4) ? cout << "c4 is true\n" : cout << "c4 is false\n";
-    assert(c4);
-
-    /*
-     * The next line of code will implicitly
-     * construct a `Complex` out of 14.2 using the default
-     * imaginary component of 0.0.
-     * */
-    Complex c5 = 14.2;
-    cout << c5 << endl;
-    assert(c5.get_real() == 14.2);
-    Complex scalar_prod = c5 * 2;
-    assert(scalar_prod.get_real() == 28.4);
-    assert(scalar_prod.get_imag() == 0.0);
+//    if(DEBUG)
+//    {
+//        cout << "c1 = " << c1 << endl;
+//    }
+//
+//    Complex c2{43.2, 58.9};
+//    assert(c2.get_real() == 43.2);
+//    assert(c2.get_imag() == 58.9);
+//
+//    if(DEBUG)
+//    {
+//        cout << "c2 = " << c2 << endl;
+//    }
+//
+//    // see if `Complex` addition works:
+//    Complex c3 = c1 + c2;
+//    assert(c3.get_real() > 43.2);
+//    // see if `Complex` subtraction works:
+//    Complex c3 -= c2;
+//    assert(c3 == c1);
+//
+//    Complex c4 = Complex();
+//    // test pre-increment:
+//    ++c4;
+//    ++c4;
+//    assert(c4.get_real() == 2);
+//    Complex postinc = c4++;
+//    assert(postinc.get_real() == 2);
+//    postinc--;
+//    assert(postinc.get_real() == 1);
+//    /*
+//     * Test equality operator. If we take the `explicit` off of
+//     * `bool()` in class definition, then this will fail as ambiguous:
+//     * the compiler won't know if we want a `bool` or `Complex` comparison.
+//     * */
+//    if (1 == bool(c4))
+//        cout << "c4 == 1 is true\n";
+//    else
+//        cout << "c4 == 1 is false\n";
+//    // test bool() operator:
+//    if (c4) {
+//        cout << "c4 is true\n";
+//    }
+//    else {
+//        cout << "c4 is false\n";
+//    }
+//    assert(c4);
+//
+//    /*
+//     * The next line of code will implicitly
+//     * construct a `Complex` out of 14.2 using the default
+//     * imaginary component of 0.0.
+//     * */
+//    Complex c5 = 14.2;
+//    cout << c5 << endl;
+//    assert(c5.get_real() == 14.2);
+//    Complex scalar_prod = c5 * 2;
+//    assert(scalar_prod.get_real() == 28.4);
+//    assert(scalar_prod.get_imag() == 0.0);
 }
