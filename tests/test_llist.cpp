fb4830384d58a7db2f196629e12e561300e92d37
gcallah <gcallah@mac.com>
2019-06-20 11:24:08 -0400

    Checking in for ll lab 1.

--- tests/test_llist.cpp
+++ tests/test_llist.cpp
@@ -5,18 +5,26 @@ using namespace std;
 
 int main() {
     Node* head = nullptr;
+    head = new Node(8, nullptr);
     add_at_end(head, 16);
     add_at_end(head, 32);
     add_at_end(head, 64);
     add_at_end(head, 128);
     add_at_end(head, 256);
-    assert(head->data == 16);
+    assert(head->data == 8);
 
     // let's see if our last() function works:
     Node* lastp = last(head);
     assert(lastp->data == 256);
     cout << "Last: " << lastp << endl;
 
+    add_at_front(head, 4);
+    assert(head->data == 4);
+
+    print_list(cout, head);
+
+    /*
     cout << "List: \n";
     print_list(cout, head);
+    */
 }
