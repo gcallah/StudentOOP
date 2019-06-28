34e6d073bd1786e05719d81f834498d8c16f6c14
gcallah <gcallah@mac.com>
2019-06-28 09:22:51 -0400

    bst ready for lab.

--- my_code/bst.h
+++ my_code/bst.h
@@ -1,29 +1,72 @@
 #include <iostream>
 
+void indent(int level) {
+    // indent more at each level:
+    for (int i = 0; i < level; i++) std::cout << "  ";
+}
+
 template <typename T>
 class Bst {
-    friend void print_bst(const Bst<T>& bst) {
-        std::cout << "data: " << bst.data << "; left: ";
+    friend void print_bst(const Bst<T>& bst, int level=0) {
+        indent(level);
+        level++;
+
+        std::cout << "data: " << bst.data << std::endl;
+        indent(level + 1);
+        std::cout << "left: ";
    
-        if (!bst.left) std::cout << "nullptr ";
+        if (!bst.left) std::cout << "nullptr\n";
         else {
-            std::cout << "\t\n";
-            print_bst(*bst.left);
+            std::cout << "\n";
+            indent(level);
+            print_bst(*bst.left, level);
         }
-        std::cout << "; right: ";
-        if (!bst.right) std::cout << "nullptr ";
+        indent(level + 1);
+        std::cout << "right: ";
+        if (!bst.right) std::cout << "nullptr\n";
         else {
-            std::cout << "\t\n";
-            print_bst(*bst.right);
+            std::cout << "\n";
+            indent(level);
+            print_bst(*bst.right, level);
         }
         std::cout << std::endl;
     }
 
     public:
-        Bst(T d, Bst* l=nullptr, Bst* r=nullptr)
-            : data(d), left(l), right(r) {}
+        Bst(T d, Bst* p=nullptr, Bst* l=nullptr, Bst* r=nullptr)
+            : data(d), parent(p), left(l), right(r) {}
+
+        Bst<T>* insert(const T d) {
+            if (d == data) return nullptr;
+            else if (d < data) {
+                if (!left) {
+                    left = new Bst(d, this);
+                    return left;
+                }
+                else {
+                    return left->insert(d);
+                }
+            }
+            else {   // d > data
+                if (!right) {
+                    right = new Bst(d, this);
+                    return right;
+                }
+                else {
+                    return right->insert(d);
+                }
+            }
+        }
+
+        T get_val() const { return data; }
+
+        Bst<T>* find(const T d) const {
+            return nullptr;
+        }
+
     private:
         T data;
+        Bst* parent;
         Bst* left;
         Bst* right;
 };
--- tests/test_bst.cpp
+++ tests/test_bst.cpp
@@ -1,9 +1,49 @@
+
+/*
+ * Binary search tree algorithms in C++.
+ * An overview of these algorithms can be found here:
+ * http://www.zrzahid.com/binary-search-tree-bst-insert-delete-successor-predecessor-traversal/
+ * You must fill in bst.h to pass all tests below:
+ * */
+
 #include <cassert>
 #include "../my_code/bst.h"
 
 using namespace std;
 
 int main() {
-    Bst<int>* iroot = new Bst<int>(2);
+    Bst<int>* iroot = new Bst<int>(40);
+    assert(iroot->get_val() == 40);
     print_bst(*iroot);
+
+    iroot->insert(20);
+    iroot->insert(60);
+    print_bst(*iroot);
+    // assert(min(iroot) == 20);
+    // assert(max(iroot) == 60);
+
+    iroot->insert(10);
+    iroot->insert(30);
+    iroot->insert(50);
+    iroot->insert(70);
+    print_bst(*iroot);
+    // assert(min(iroot) == 10);
+    // assert(max(iroot) == 70);
+
+    iroot->insert(5);
+    iroot->insert(25);
+    iroot->insert(45);
+    iroot->insert(75);
+    print_bst(*iroot);
+    // assert(min(iroot) == 5);
+    // assert(max(iroot) == 75);
+
+    /*
+    Bst<int>* result = iroot->search(45);
+    assert(result->get_val() == 45);
+    result = iroot->search(95);
+    assert(result == nullptr);
+
+    
+     */
 }
